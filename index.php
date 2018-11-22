529ba0cbf33591aedebe43a4343000f51aaf4393
Francesco Bianco <info.francescobianco@gmail.com>
2018-11-22 19:11:16 +0100

    asdasd

--- index.php
+++ index.php
@@ -3,26 +3,44 @@
  * javanile/ipqueue (v0.0.1)
  */
 
-$json = [ 'Request' => getallheaders() ];
-$json['Request']['Method'] = $_SERVER['REQUEST_METHOD'];
-$json['Service'] = [ 'Name' => substr($json['Request']['Host'], 0, strpos($json['Request']['Host'], '.')) ];
+$json = [ 'request' => [] ];
 
-if (strlen($json['Service']['Name']) < 4) {
-    $json['Message'] = [ 'Type' => 'Error', 'Text' => 'Service name is too short' ];
+foreach (getallheaders() as $key => $value) {
+    $json['request'][strtolower($key)] = $value;
+}
+
+$json['request']['method'] = $_SERVER['REQUEST_METHOD'];
+$json['service'] = [ 'name' => substr($json['request']['host'], 0, strpos($json['request']['host'], '.')) ];
+
+if (strlen($json['service']['name']) < 4) {
+    $json['message'] = [ 'type' => 'error', 'text' => 'Service name is too short' ];
     echo json_encode($json, JSON_PRETTY_PRINT)."\n";
     exit(1);
 }
 
-switch ($json['Request']['Method']) {
+$path = __DIR__.'/data/'.$json['service']['name'][0].'/'.$json['service']['name'][1];
+$file = $path.'/'.$json['service']['name'].'.json';
+$have = file_exists($file);
+
+switch ($json['request']['method']) {
     case 'GET':
+        if ($have) {
+            echo file_get_contents($file);
+        }
+        exit(0);
         break;
 
     case 'POST':
-        $json['Service']['Ip'] = $_SERVER['REMOTE_ADDR'];
+        $json['service']['ip'] = $_SERVER['REMOTE_ADDR'];
+        if (!$have && !is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+        file_put_contents($file, $json['service']['ip']);
+        $json['message'] = [ 'type' => 'Success', 'text' => 'service updated' ];
         break;
 
     default:
-        $json['Message'] = [ 'Type' => 'Error', 'Text' => 'Unsupported HTTP method: '.$json['Request']['Method'] ];
+        $json['message'] = [ 'type' => 'Error', 'text' => 'Unsupported HTTP method: '.$json['request']['method'] ];
         break;
 }
 
