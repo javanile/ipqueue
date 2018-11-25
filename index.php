7dce00091a0e375a09a90cda5404c5caebb07b6e
Francesco Bianco <info.francescobianco@gmail.com>
2018-11-25 01:18:14 +0100

    asd

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
  * javanile/ipqueue (v0.0.1)
  */
 
-$json = [ 'request' => [] ];
+define('IPQUEUE_HOST', 'www.ipqueue.com');
 
+$json = [ 'request' => [] ];
 foreach (getallheaders() as $key => $value) {
     $json['request'][strtolower($key)] = $value;
+}
+
+if ($json['request']['host'] == IPQUEUE_HOST) {
+    require_once __DIR__.'/public/index.php';
+    exit(0);
 }
 
 $json['request']['method'] = $_SERVER['REQUEST_METHOD'];
@@ -36,11 +42,11 @@ switch ($json['request']['method']) {
             mkdir($path, 0777, true);
         }
         file_put_contents($file, $json['service']['ip']);
-        $json['message'] = [ 'type' => 'Success', 'text' => 'service updated' ];
+        $json['message'] = [ 'type' => 'success', 'text' => 'Service successful updated' ];
         break;
 
     default:
-        $json['message'] = [ 'type' => 'Error', 'text' => 'Unsupported HTTP method: '.$json['request']['method'] ];
+        $json['message'] = [ 'type' => 'error', 'text' => 'Unsupported HTTP method: '.$json['request']['method'] ];
         break;
 }
 
