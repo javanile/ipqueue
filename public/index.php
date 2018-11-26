2ae67d2e2bac32393725fea1b98262c372884944
Francesco Bianco <info.francescobianco@gmail.com>
2018-11-26 15:18:17 +0100

    commit nre lòi

--- public/index.php
+++ public/index.php
@@ -24,12 +24,18 @@
 </style>
 </head>
 <body>
-<a href="https://github.com/javanile/ipqueue"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
+<a href="https://github.com/javanile/ipqueue">
+    <img style="position: absolute; top: 0; right: 0; border: 0;"
+         src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub">
+</a>
 <article class="markdown-body">
     <h1>
-        <span class="pl-ms">ip</span>queue<br/>
-        <small class="pl-mi">Service discovery over HTTP API</small>
+        <span class="pl-ms">ip</span>queue
+        <small>Service discovery over HTTP API</small>
     </h1>
+
+
+
 
     <p>All the things</p>
 
